8d7f5108768de18c5a834f15c8b1db510eb2650e
Dayong Hyun <46876637+gusek93@users.noreply.github.com>
2022-05-10 14:28:51 +0900

    first commit

---
+++ src/main/java/com/example/springjapexample/SpringJapExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjapexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJapExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJapExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springjapexample/SpringJapExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjapexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJapExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
